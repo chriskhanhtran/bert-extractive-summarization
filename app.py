b468f37d8cb5f211d30d12ccc0216bc541fe5bda
chriskhanhtran <tranduckhanh96@gmail.com>
2020-05-24 20:05:15 +0000

    change download_model

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 import streamlit as st
 import os
 import torch
+import nltk
 import urllib.request
 from models.model_builder import ExtSummarizer
 from newspaper import Article
 from ext_sum import summarize
-import nltk
 
-
-@st.cache(suppress_st_warning=True)
-def download_model():
-    nltk.download('popular')
-    url = 'https://www.googleapis.com/drive/v3/files/1tXdugYx8NU73_G4FK7XX08aGip_D1SiJ?alt=media&key=AIzaSyCmo6sAQ37OK8DK4wnT94PoLx5lx-7VTDE'
-    urllib.request.urlretrieve(url, 'checkpoint/cnndm_ext.pt')
-
-@st.cache(suppress_st_warning=True)
-def load_model(path):
-    checkpoint = torch.load(path)
-    model = ExtSummarizer(device="cpu", checkpoint=checkpoint, max_pos=512)
-    return model
-
-def crawl_url(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article.text
 
 def main():
     st.markdown("<h1 style='text-align: center;'>Extractive Summary✏️</h1>", unsafe_allow_html=True)
@@ -59,6 +41,55 @@ def main():
     summary = summarize(input_fp, result_fp, model, max_length=max_length)
     st.markdown("<h3 style='text-align: center;'>Summary</h3>", unsafe_allow_html=True)
     st.markdown(f"<p align='justify'>{summary}</p>", unsafe_allow_html=True)
+
+
+def download_model():
+    nltk.download('popular')
+    url = 'https://www.googleapis.com/drive/v3/files/1tXdugYx8NU73_G4FK7XX08aGip_D1SiJ?alt=media&key=AIzaSyCmo6sAQ37OK8DK4wnT94PoLx5lx-7VTDE'
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading checkpoint...")
+        progress_bar = st.progress(0)
+        with open('checkpoint/cnndm_ext.pt', 'wb') as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning("Downloading checkpoint... (%6.2f/%6.2f MB)" %
+                        (counter / MEGABYTES, length / MEGABYTES))
+                    progress_bar.progress(min(counter / length, 1.0))
+
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
+@st.cache(suppress_st_warning=True)
+def load_model(path):
+    checkpoint = torch.load(path)
+    model = ExtSummarizer(device="cpu", checkpoint=checkpoint, max_pos=512)
+    return model
+
+
+def crawl_url(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
 
 if __name__ == "__main__":
     main()
